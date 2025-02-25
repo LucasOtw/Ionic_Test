0a8d0a5f80a90a10e9eb60fa8485532afef0482e
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2025-02-25 21:52:41 +0100

    ajout de nouveaux details pour un trip

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { IonCard, IonCardContent, IonCardTitle, IonCardSubtitle } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardTitle, IonCardSubtitle, IonContent, IonText } from '@ionic/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-regular-svg-icons';
 import './test.css';  // Pour le CSS personnalisé
 
 const Example: React.FC = () => {
@@ -10,14 +12,30 @@ const Example: React.FC = () => {
           <img alt="Image d'offre" src="/assets/images/Barcelone.jpg" className="card-image" />
           <div className="card-image-overlay"></div>
         </div>
+
+        <IonCardContent className="card-text-save">
+          <div className="card-image-save">
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </IonCardContent>
         <IonCardContent className="card-text-content">
-        <div className='tag-container'>
-          <IonCardSubtitle className='tag-card'>Populaire 🔥</IonCardSubtitle>
+          <div className='tag-container'>
+            <IonCardSubtitle className='tag-card'>Populaire 🔥</IonCardSubtitle>
           </div>
           <IonCardTitle>Barcelone</IonCardTitle>
           <IonCardSubtitle>Espagne 🇪🇸</IonCardSubtitle>
         </IonCardContent>
       </IonCard>
+
+      <IonContent>
+        <IonText>
+          <div className='detail_city'>
+            <p>Ensoleiller</p>
+            <p>Chaud</p>
+            <p>1.6mio Hab</p>
+          </div>
+        </IonText>
+      </IonContent>
     </>
   );
 }
